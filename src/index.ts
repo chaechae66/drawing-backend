cd8c3680bc6048b52dfa9f24e57cd4be44c5449b
chaechae66 <valueworkerchae@gmail.com>
2024-03-22 14:20:43 +0900

    Feat: 기본 셋팅 및 라우터 적용

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
-function numReturn(num: Number) {
-  return num;
-}
+let mongoose = require("mongoose"),
+  express = require("express"),
+  bodyParser = require("body-parser"),
+  cors = require("cors");
 
-console.log(numReturn(5));
+require("dotenv").config();
+
+const article = require("./routes/article");
+
+const connection = process.env.mongodbURL;
+
+mongoose
+  .connect(connection)
+  .then(() => console.log("Database Connected Successfully"))
+  .catch((err) => console.log(err));
+
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extends: true,
+  })
+);
+app.use(cors());
+app.use("/article", article);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log("Connected to port " + port);
+});
+app.use(function (err, req, res, next) {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});
+export {};
